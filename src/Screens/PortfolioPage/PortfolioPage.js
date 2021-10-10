549d7162ef6abab29fc6a8e94b594220cc9c1c48
Bogdan <bogdanmarcu2003@gmail.com>
2021-10-10 20:05:54 +0300

    bogdan2

--- src/Screens/PortfolioPage/PortfolioPage.js
+++ src/Screens/PortfolioPage/PortfolioPage.js
@@ -1,25 +1,27 @@
 import React from "react";
 import ClientCard from "../../Components/ClientCard/ClientCard";
-import './PortfolioPage.css'
+import "./PortfolioPage.css";
 import Clients from "../../Components/ClientsArray";
 import Scroll from "../../Components/Scroll";
 
 export default function PortfolioPage() {
     return (
         <div className="screen-component">
-         <h2 className="portofolio-title"> Portofoliu</h2>
-         {/* <Scroll> */}
+            <h2 className="portofolio-title"> Portofoliu</h2>
+            {/* <Scroll> */}
             <div className="portofolio-list-outside">
                 <div className="clients_grid">
                     {Clients.map((client, index) => {
-                        return <ClientCard className="client_div" img={client.src}></ClientCard>;
+                        return (
+                            <ClientCard
+                                className="client_div"
+                                img={client.src}
+                            ></ClientCard>
+                        );
                     })}
                 </div>
             </div>
-        {/* </Scroll> */}
+            {/* </Scroll> */}
         </div>
     );
 }
-
-
-
