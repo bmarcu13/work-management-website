c7ab5805a65e3034d8f65442c57bd6aac84e2aa5
Catalina <petruscatalina@yahoo.com>
2021-10-14 21:07:05 +0300

    re-styled about us

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -10,33 +10,38 @@ export default function AboutUs() {
           <hr></hr>
             <div className="first-container">
                 <div className="who-we-are-text">
-                    <h2 class="contentHead">CINE SUNTEM?</h2>
-                    <h4>
-                    WORK MANAGEMENT este abilitata, conform certificatelor de abilitare nr.1312/PS din 24.02.2021 emis de Ministerul Muncii, Familiei si Protectiei Sociale – ITM Bucuresti, sa desfasoare activitatii specifice de prevenire si protectie.
+                    <div class="contentHead">
+                    <h2>CINE SUNTEM?</h2>
+                    </div>
+                    <h4>WORK MANAGEMENT este abilitata, conform certificatelor de abilitare nr.1312/PS din 24.02.2021 emis de Ministerul Muncii, Familiei si Protectiei Sociale – ITM Bucuresti, sa desfasoare activitatii specifice de prevenire si protectie.
                         <br/><br/>
-                        Something 
-                        <br/><br/>
+                        Something<br/><br/>
                         <p>(NOTICE:)</p>
                         <p><span>Anything</span></p>
                     </h4>
-                                    </div>
-                                    
-                                    <div class="pic"></div>
+                </div>
+                <div class="pic"></div>
+            </div>
+                <div className="second-container">
+                    <div className="iso-images">
+                        <div className="first-row-certificates">
+                            <img className="iso-img" src={Iso_blue} alt="iso_blue" style={{width:'150px', height:'150px'}}></img>
+                            <img className="iso-img"  src={Iso_orange} alt="iso_orange"style={{width:'150px', height:'150px'}} ></img>
+                        </div>
+                        <div className="second-row-certificates">
+                            <img className="iso-img" src={Iso_blue} alt="iso_blue" style={{width:'150px', height:'150px'}}></img>
+                            <img className="iso-img"  src={Iso_orange} alt="iso_orange" style={{width:'150px', height:'150px'}}></img>
+                        </div>
                     </div>
-                                <div className="second-container">
-                                    <div className="iso-images">
-                                        <img className="iso-img" src={Iso_blue} alt="iso_blue"></img>
-                                        <img className="iso-img"  src={Iso_orange} alt="iso_orange"></img>
-                                    </div>
-                                    <div className="our-mission-text">
-                                    <h2 class="contentHead">MISIUNEA NOASTRA</h2>
-                                        <p>Abordarea aspectelor de securitate si sanatate, la nivelul societatii, va asigura nu numai evitarea costurilor mari cu accidentele si imbolnavirile profesionale, dar si imbunatatirea productivitatii muncii. </p>
-                                        <h4>WORK MANAGEMENT este certificata ISO 9001:2015, ISO 14001:2015, ISO 45001:2018, ISO 27001:2018.</h4>
-                                        <h5>Deviza echipei de management este: <br></br><span>
-                        „Celor care spun ca securitatea in munca costa le-as spune ca lipsa acesteia costa si mai mult.”</span>
-                    </h5>
+                    <div className="our-mission-text">
+                        <h2 class="contentHead">MISIUNEA NOASTRA</h2>
+                        <p>Abordarea aspectelor de securitate si sanatate, la nivelul societatii, va asigura nu numai evitarea costurilor mari cu accidentele si imbolnavirile profesionale, dar si imbunatatirea productivitatii muncii. </p>
+                        <h4>WORK MANAGEMENT este certificata ISO 9001:2015, ISO 14001:2015, ISO 45001:2018, ISO 27001:2018.</h4>
+                        <h5>Deviza echipei de management este: <br></br><span>
+                            „Celor care spun ca securitatea in munca costa le-as spune ca lipsa acesteia costa si mai mult.”</span>
+                        </h5>
+                    </div>
                 </div>
             </div>
-        </div>
         );
 }
