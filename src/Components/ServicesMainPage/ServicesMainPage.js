9e9eeb3b58a1663a1ffba984910abca021d24442
Bogdan <bogdanmarcu2003@gmail.com>
2021-10-18 20:37:28 +0300

    - Design changes for services section in landing page - Design changes in portfolio page

--- src/Components/ServicesMainPage/ServicesMainPage.js
+++ src/Components/ServicesMainPage/ServicesMainPage.js
@@ -3,16 +3,50 @@ import "./ServicesMainPage.css";
 
 import protectiaMunciiPsiImg from "../../img/services/protectia-muncii-psi.jpg";
 
-import { MdOutlineHorizontalRule } from "react-icons/md";
+import {
+    MdOutlineHorizontalRule,
+    MdOutlineArrowForwardIos,
+} from "react-icons/md";
+
+import SecuritateSanatateInMunca from "../../img/services/protectia-muncii-psi.jpg";
+import CoordonareaSantierelor from "../../img/services/coordonarea-santierelor.png";
+import SituatiiUrgenta from "../../img/services/situatii-urgenta.jpg";
+import Deseuri from "../../img/services/deseuri.jpg";
+import RSVTI from "../../img/services/rsvti.jpg";
+import AnalizaRiscuri from "../../img/services/analiza-riscuri.jpg";
+
+import DividerPhoto from "../../img/landing-page-services-divider-photo.jpg";
 
 export default function ServicesMainPage() {
     return (
         <div className="services">
-            <h2 style={{}}>Serviciile noastre</h2>
+            <div className="divider">
+                <img src={DividerPhoto} />
+                <div
+                    style={{
+                        backgroundColor: "black",
+                        position: "absolute",
+                        opacity: ".4",
+                        height: "200px",
+                        width: "100%",
+                    }}
+                ></div>
+                <h1 style={{ position: "absolute" }}>
+                    Serviciile Noastre
+                    <hr></hr>
+                </h1>
+            </div>
+
             <div className="grid-container">
                 {servicesList.map((item, index) => {
                     return gridItemComponent(item);
                 })}
+            </div>
+            <div className="all-services-button">
+                <h3 style={{ display: "flex" }}>
+                    Toate Serviciile
+                    <MdOutlineArrowForwardIos style={{ alignSelf: "center" }} />
+                </h3>
             </div>
         </div>
     );
@@ -22,17 +56,10 @@ const gridItemComponent = (item) => {
     return (
         <div className="grid-item-container">
             <div className="img-container">
-                <img src={protectiaMunciiPsiImg}></img>
+                <img src={item.img}></img>
             </div>
-            <h3>Service name placeholder</h3>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-                gravida, urna quis pulvinar lacinia, nulla leo molestie orci,
-                eget pellentesque lacus dolor nec odio. Sed vulputate enim
-                tellus, suscipit tempor dolor interdum vel. Fusce semper
-                dignissim augue, sit amet ultrices ex sollicitudin nec. Donec
-                convallis feugiat massa, a auctor massa sagittis ut.
-            </p>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
             <p
                 className="read-more-button"
                 style={{ fontWeight: "900", transition: ".2s" }}
@@ -50,31 +77,42 @@ const servicesList = [
     {
         title: "Securitate si Sanatate in Munca",
         description: "",
-        img: {},
+        img: SecuritateSanatateInMunca,
     },
     {
         title: "Situatii de Urgenta (Prevenirea si Stingerea Incendiilor / Protectie Civila)",
         description: "",
-        img: {},
+        img: SituatiiUrgenta,
     },
     {
         title: "RSVTI",
-        description: "",
-        img: {},
+        description:
+            "Compania noastra este autorizată de ISCIR, să desfășoare activități de supraveghere și" +
+            "verificare tehnică a echipamentelor/instalațiilor de ridicat și sub presiune și deține un număr de" +
+            "peste 25 ingineri specialiști în domeniul ISCIR, cu multă experiență.",
+        img: RSVTI,
     },
     {
         title: "Gestiunea Deseurilor si Managementul Mediului",
-        description: "",
-        img: {},
+        description:
+            "Ținând cont de faptul că activitatea economică are impact asupra mediului, orice operator economic, cu atât mai mult cel care, prin activitatea sa, produce diferite tipuri de deșeuri, este obligat să dețină un departament de protecția mediului" +
+            " Noi preluăm responsabilitatea gestiunii deșeurilor și vă punem la dispoziție un specialist în managementul deșeurilor.",
+        img: Deseuri,
     },
     {
         title: "Coordonarea Santierelor",
-        description: "",
-        img: {},
+        description:
+            "Sunteti beneficiarul unui proiect de constructii si aveti nevoie de un Coordonator SSM pentru" +
+            "santierul Dvs pentru a indeplini cerintele minime ale HG 300/2006?" +
+            "Sunteti o firma de proiectare si aveti nevoie de intocmirea planului de securitate si sanatate" +
+            "pentru proiectele Dvs?",
+        img: CoordonareaSantierelor,
     },
     {
         title: "Analiza de Risc la Securitate Fizica",
-        description: "",
-        img: {},
+        description:
+            "Identificam si evaluam riscurile profesionale la care sunt expusi angajatii la locurile de munca si intocmim planul de prevenire si protectie pe baza acestora." +
+            "Scopul evaluării nivelului de risc este de a oferi posibilitatea cunoaşterii situaţiei reale, de la fiecare loc de muncă, din punct de vedere al securităţii şi sănătăţii în muncă, pentru a se lua măsurile de prevenire optime şi realiste.",
+        img: AnalizaRiscuri,
     },
 ];
--- src/Screens/PortfolioPage/PortfolioPage.js
+++ src/Screens/PortfolioPage/PortfolioPage.js
@@ -5,10 +5,39 @@ import "./PortfolioPage.css";
 import Clients from "../../Components/ClientsArray";
 import Scroll from "../../Components/Scroll";
 
+import headerBanner from "../../img/our-clients-banner.png";
+
 export default function PortfolioPage() {
     return (
         <div className="screen-component">
-            <h2 className="portofolio-title"> Portofoliu</h2>
+            <div className="portfolio-header">
+                <img src={headerBanner} />
+                <div
+                    style={{
+                        backgroundColor: "black",
+                        top: "0",
+                        position: "absolute",
+                        opacity: ".4",
+                        height: "350px",
+                        width: "100%",
+                    }}
+                ></div>
+                <h2> Portofoliu Clienti</h2>
+                <div class="custom-shape-divider-bottom-1634577933">
+                    <svg
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1200 120"
+                        preserveAspectRatio="none"
+                    >
+                        <path
+                            d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
+                            class="shape-fill"
+                        ></path>
+                    </svg>
+                </div>
+            </div>
+
             {/* <Scroll> */}
             <div className="portofolio-list-outside">
                 <div className="clients_grid">
