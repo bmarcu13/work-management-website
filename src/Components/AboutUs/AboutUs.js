2ad99af3f479e25e9b331a1a8e5310c6cc44eba5
Catalina <petruscatalina@yahoo.com>
2021-10-23 15:05:37 +0300

    added logo.ico

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -13,12 +13,12 @@ export default function AboutUs() {
                     <div class="contentHead">
                     <h2>CINE SUNTEM?</h2>
                     </div>
-                    <h4>WORK MANAGEMENT este abilitata, conform certificatelor de abilitare nr.1312/PS din 24.02.2021 emis de Ministerul Muncii, Familiei si Protectiei Sociale – ITM Bucuresti, sa desfasoare activitatii specifice de prevenire si protectie.
+                    <p>WORK MANAGEMENT este abilitata, conform certificatelor de abilitare nr.1312/PS din 24.02.2021 emis de Ministerul Muncii, Familiei si Protectiei Sociale – ITM Bucuresti, sa desfasoare activitatii specifice de prevenire si protectie.
                         <br/><br/>
                         Something<br/><br/>
                         <p>(NOTICE:)</p>
                         <p><span>Anything</span></p>
-                    </h4>
+                    </p>
                 </div>
                 <div class="pic"></div>
             </div>
@@ -26,6 +26,7 @@ export default function AboutUs() {
                     <div className="iso-images">
                         <div className="first-row-certificates">
                             <img className="iso-img" src={Iso_blue} alt="iso_blue" style={{width:'150px', height:'150px'}}></img>
+                            {/* <div className="pic1"></div> */}
                             <img className="iso-img"  src={Iso_orange} alt="iso_orange"style={{width:'150px', height:'150px'}} ></img>
                         </div>
                         <div className="second-row-certificates">
@@ -36,6 +37,8 @@ export default function AboutUs() {
                     <div className="our-mission-text">
                         <h2 class="contentHead">MISIUNEA NOASTRA</h2>
                         <p>Abordarea aspectelor de securitate si sanatate, la nivelul societatii, va asigura nu numai evitarea costurilor mari cu accidentele si imbolnavirile profesionale, dar si imbunatatirea productivitatii muncii. </p>
+                        <p>Lucratorii bine motivati si bine instruiti, lucrand cu echipamente si materiale eficiente si bine intretinute, pot obtine o calitate mai buna, o
+productivitate mai buna si performante economice mai bune.</p>
                         <h4>WORK MANAGEMENT este certificata ISO 9001:2015, ISO 14001:2015, ISO 45001:2018, ISO 27001:2018.</h4>
                         <h5>Deviza echipei de management este: <br></br><span>
                             „Celor care spun ca securitatea in munca costa le-as spune ca lipsa acesteia costa si mai mult.”</span>
