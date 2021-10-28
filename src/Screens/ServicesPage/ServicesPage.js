febcf9a12a26185e3567ec0466dc55fee4cf8dce
Bogdan <bogdanmarcu2003@gmail.com>
2021-10-28 19:46:51 +0300

    Created files

---
+++ src/Screens/ServicesPage/ServicesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./ServicesPage.css";
+
+export default function ServicesPage() {
+    return (
+        <div className="services-page-container">
+            <div className="grid-wrapper"></div>
+        </div>
+    );
+}
