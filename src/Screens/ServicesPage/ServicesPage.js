1752c8d1ed09acf0c2ecae05eff112066dd79dbc
Bogdan <bogdanmarcu2003@gmail.com>
2021-10-29 11:32:53 +0300

    Implemented design intended for mainpage on the services page 😎

--- src/Screens/ServicesPage/ServicesPage.js
+++ src/Screens/ServicesPage/ServicesPage.js
@@ -11,65 +11,79 @@ import { TiArrowSortedUp } from "react-icons/ti";
 
 let menuItems = [].concat(dropdownItemsLeft).concat(dropdownItemsRight);
 
-const showDropdown = (index) => {
-    const id = "gridItem" + index;
-
-    const clickeditem = document.getElementById(id);
-    const boundingRect = clickeditem.getBoundingClientRect();
-
-    const arrow = clickeditem.children[1];
-    const dropdown = clickeditem.children[2];
-
-    if (dropdown.style.height == "0px") {
-        console.log("O IUBESC PE DANA");
-
-        arrow.style.height = "50px";
-        dropdown.style.height = "200px";
-        dropdown.style.opacity = "1";
-        arrow.style.opacity = "1";
-    } else {
-        dropdown.style.height = "0px";
-        arrow.style.opacity = "0";
-        dropdown.style.opacity = "0";
-        arrow.style.left = Math.abs(boundingRect.width / 2 - 25) + "px";
-        dropdown.style.height = "0px";
-    }
-
-    console.log(dropdown);
-};
-
 export default function ServicesPage() {
     return (
         <div className="services-page-container">
-            <div className="grid-wrapper">
-                <GridItems></GridItems>
+            <div className="cards-container">
+                <div className="title-section">
+                    <TiArrowSortedUp
+                        color={"white"}
+                        size={60}
+                        style={{
+                            position: "absolute",
+                            right: -26,
+                            transform: "rotate(-90deg)",
+                            top: "calc(50% - 30px)",
+                        }}
+                    ></TiArrowSortedUp>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            margin: "auto",
+                        }}
+                    >
+                        <h2 style={{ color: "white", margin: "auto" }}>
+                            Serviciile noastre
+                        </h2>
+                        <p
+                            style={{
+                                color: "white",
+                                margin: "10px 20px auto",
+                                textAlign: "center",
+                            }}
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Eget mauris pharetra et
+                            ultrices neque ornare aenean euismod elementum. Amet
+                            dictum sit amet justo. Pretium quam vulputate
+                            dignissim suspendisse in est.
+                        </p>
+                    </div>
+                    <button
+                        style={{
+                            marginBottom: "auto",
+                            width: "150px",
+                            height: "40px",
+                            backgroundColor: "orange",
+                            border: "none",
+                            borderRadius: "20px",
+                            fontSize: "16px",
+                            fontWeight: "bold",
+                            color: "white",
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                        }}
+                    >
+                        Toate serviciile
+                    </button>
+                </div>
+
+                <div className="card-section">
+                    <CardGrid></CardGrid>
+                </div>
             </div>
         </div>
     );
 }
 
-const GridItems = () => {
-    return menuItems.map((item, index) => {
-        return <ListItem itemData={item} index={index}></ListItem>;
+const CardGrid = () => {
+    return dropdownItemsLeft.map((data, index) => {
+        return <Card cardData={data}></Card>;
     });
 };
 
-const ListItem = ({ itemData, index }) => {
-    const id = "gridItem" + index;
-
-    return (
-        <div id={id} className="grid-element-wrapper">
-            <div
-                style={{ width: "100%" }}
-                onClick={() => showDropdown(index)}
-                className="card-container"
-            >
-                SA MOARA DANA IN PUSCARIE DE NU MERGI SA MOARA DANA IN PUSCARIE
-                DE NU MERGI SA MOARA DANA IN PUSCARIE DE NU MERGI SA MOARA DANA
-                IN PUSCARIE DE NU MERGI SA MOARA DANA IN PUSCARIE DE NU MERGI
-            </div>
-            <TiArrowSortedUp className="dropdown-arrow" size={50} />
-            <div className="dropdown"></div>
-        </div>
-    );
+const Card = ({ cardData }) => {
+    return <div className="card-wrapper">DANA!</div>;
 };
