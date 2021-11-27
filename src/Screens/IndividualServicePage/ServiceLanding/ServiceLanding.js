94a08d7889e184596327657f000110e062a66ba3
Bogdan <bogdanmarcu2003@gmail.com>
2021-11-27 19:27:07 +0200

    implemented cool bg

--- src/Screens/IndividualServicePage/ServiceLanding/ServiceLanding.js
+++ src/Screens/IndividualServicePage/ServiceLanding/ServiceLanding.js
@@ -1,12 +1,13 @@
 import React from "react";
 import oras from "../../../img/oras.jpg";
+import cercblana from "../../../img/cerc_blana.svg";
 
 import "./ServiceLanding.css";
 
 export default function ServiceLanding(props) {
     return (
         <div className="service-page-landing-container">
-            <img src={oras} />
+            <div className="svg-bg"></div>
         </div>
     );
 }
