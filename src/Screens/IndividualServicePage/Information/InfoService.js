7294ae730bd6a809f5e5a10af2af3e7672b52546
Catalina <petruscatalina@yahoo.com>
2021-12-23 23:36:41 +0200

    added all info and some styles

--- src/Screens/IndividualServicePage/Information/InfoService.js
+++ src/Screens/IndividualServicePage/Information/InfoService.js
@@ -49,12 +49,12 @@ export const servicesInfo = [
                 text: "Întocmim toată documentația necesară serviciilor de personal.",
             },
         ],
-        info_title:"",
-        information: "a. organizarea activității de securitate și sănătate în muncă, la nivelul angajatorului și la nivelul fiecărui loc de muncă din organizație; "+ '\n'+
+        information: '#style{"fontWeight":"bold", "fontSize": 22}Serviciile noastre SSM includ#/:\n\n'+
+        "a. organizarea activității de securitate și sănătate în muncă, la nivelul angajatorului și la nivelul fiecărui loc de muncă din organizație; "+ '\n'+
         "b. pregătirea și instruirea lucrătorilor:\n"+
-        '\t'+"    i. la angajare;\u000A"+
-        "\t         ii. la locul de muncă;\u000A"+
-        "   iii. periodic;\u000A"+ 
+        ' #style{"marginLeft":"30px"}  i. la angajare;#/\u000A'+
+        ' #style{"marginLeft":"30px"} ii. la locul de muncă;\u000A'+
+        ' #style{"marginLeft":"30px"}  iii. periodic;\u000A'+ 
         "c. verificare, prin teste, a cunoștințelor dobândite în domeniul securității și sănătății în muncă;\n"+
         "d. consultanță, privind metode și mijloace de propagandă, referitoare la securitatea și sănătatea în muncă a lucrătorilor;\n"+
         "e. asistență la controalele efectuate de către I.T.M., privind activitatea de securitate și sănătate în muncă, dar și la cercetarea și întocmirea dosarului, în cazul producerii accidentelor de muncă;\n"+
@@ -202,7 +202,7 @@ export const servicesInfo = [
                 text: "Comunicare permanentă între beneficiar și coordonatorul în materie de securitate şi sănătate.",
             },
         ],
-        information: "Vă oferim:\n"+
+        information: '#style{"fontWeight":"bold", "fontSize": 21}Vă oferim:#/\n\n'+
         "• Servicii de Coordonare SSM prin serviciu extern de prevenire și protecție astfel încât să vă conformați în totalitate cu HG 300/2006 privind cerinţele minime de securitate şi sănătate pe şantierele temporare şi mobile care stipulează că obligativitate, desemnarea unui coordonator de securitate şi elaborarea planului de securitate și sănătate atât în faza de întocmire a proiectului cât și pe perioada execuției lucrărilor.\n"+
         "• Întocmire Plan de Securitate și Sănătate în faza de proiectare\n"+
         "• Întocmire Plan de Securitate și Sănătate în faza de execuție\n"+
@@ -228,8 +228,11 @@ export const servicesInfo = [
                 text: "În caz contrar riscați amenzi uriașe! (5000-10000 RON)",
             },
         ],
-        information: "",
-    },
+        information: "Obligativitatea analizei de risc, este prevazută, de Legea 333/2003 privind paza obiectivelor, bunurilor, valorilor și protecția persoanelor, pentru care s-au emis în anul 2012 normele de aplicare, prin hotărârea de guvern 301/2012.\n"+
+        "Evaluarea de risc, la securitatea fizică, este documentul, prin care, un specialist analizează și stabilește, de ce anume este nevoie, pentru ca paza obiectivelor dumneavoastră, să acopere toate vulnerabilitatile acestuia, în fața hoților.\n"+
+        "Toate societățile comerciale, sunt obligate să contracteze, un evaluator de risc, la securitat fizică, care să realizeze analiza de risc. Aceasta urmează să fie efectuată, pentru toate obiectele de activitate active și sediile dumneavoastră.\n\n"+
+        'Toate societățile comerciale, sunt obligate să contracteze, un evaluator de risc, la securitate fizică, care să realizeze analiză de risc. Aceasta urmează să fie efectuată, pentru toate obiectele de activitate active și sediile dumneavoastră. \n\n Societatea noastră, este abilitată, să întocmească astfel de evaluări, fiind înscrisă, în #style{"fontWeight":"bold"}Registrul Național al Evaluatorilor de Risc, la Securitate Fizică#/',
+        },
     {
         id: "formare-profesionala",
         name: "Formare Profesională",
@@ -257,17 +260,18 @@ export const servicesInfo = [
         '#style{"fontWeight":"bold","fontSize":21}Competențele dobandite în urma cursului sunt:#/\n'+
         "• Realizarea activităţilor de prevenire şi protecţie\n"+
         "• Realizarea semnalizării de securitate şi/sau de sănătate la locurile de muncă\n"+
-        "•Instruirea lucrătorilor în domeniul securităţii şi sănătăţii în muncă\n"+
-        "•Informarea lucrătorilor în domeniul securităţii şi sănătăţii în muncă\n"+
-        "•Stabilirea mijloacelor materiale şi tehnice necesare securităţii şi sănătăţii în muncă\n"+
-        "•Prevenirea accidentelor de muncă şi a îmbolnăvirilor profesionale\n"+
-        "•Participarea la cercetarea evenimentelor care produc incapacitate temporară de muncă\n"+
-        "•Verificarea respectării prevederilor legale în domeniul securităţii şi sănătăţii în muncă\n"+
-        "•Monitorizarea activităţilor de evacuare şi intervenţie în situaţii de urgenţă\n"+
-        "•Dezvoltarea profesională\n"+
-        "•Planificarea activităţilor proprii",
+        "• Instruirea lucrătorilor în domeniul securităţii şi sănătăţii în muncă\n"+
+        "• Informarea lucrătorilor în domeniul securităţii şi sănătăţii în muncă\n"+
+        "• Stabilirea mijloacelor materiale şi tehnice necesare securităţii şi sănătăţii în muncă\n"+
+        "• Prevenirea accidentelor de muncă şi a îmbolnăvirilor profesionale\n"+
+        "• Participarea la cercetarea evenimentelor care produc incapacitate temporară de muncă\n"+
+        "• Verificarea respectării prevederilor legale în domeniul securităţii şi sănătăţii în muncă\n"+
+        "• Monitorizarea activităţilor de evacuare şi intervenţie în situaţii de urgenţă\n"+
+        "• Dezvoltarea profesională\n"+
+        "• Planificarea activităţilor proprii",
+
         information2: 'Organizăm #style{"fontWeight":"bold"}cursuri de specializare/perfecționare stivuitorist #/(COD COR 834403) în conformitate cu cerințele ANC si ISCIR.\n\n'+
-        "Toate sarcinile dedicate încărcării, descărcării sau deplasării pe distante scurte a unor elemente de dimensiuni și greutăți apreciabile au nevoie de îndemănare, organizare și siguranță."+
+        "Toate sarcinile dedicate încărcării, descărcării sau deplasării pe distanțe scurte a unor elemente de dimensiuni și greutăți apreciabile au nevoie de îndemănare, organizare și siguranță."+
         "În acest sens, venim în ajutorul dumneavoastră cu acest curs de specializare/perfecționare stivuitorist. Tematica acestui curs de stivuitorist este în acord cu metodologia în vigoare și respectă cele mai înalte standarde de instruire.",
     },
     {
@@ -287,10 +291,16 @@ export const servicesInfo = [
             {
                 icon: <FaRadiation size={40}></FaRadiation>,
                 name: "Prevenim",
-                text: "Efectele expunerii pe termen lung si scurt.",
+                text: "Efectele expunerii pe termen lung și scurt.",
             },
         ],
-        information: "",
+        information: '#style{"fontWeight":"bold", "fontSize": 21}Oferim\n'+
+        "Servicii de măsurare a zgomotului şi a vibraţiilor transmise omului, în regim abilitat de Ministerul Sănătăţii.\n Echipamentele utilizate pentru măsurători corespund cerinţelor standardelor internaţionale şi răspund întocmai cerinţelor minime de securitate şi sănătate la locul de muncă, respective HG 1876/2005.\n"+
+        "Pentru măsurarea vibraţiilor transmise omului este necesară cunoaşterea următoarelor informaţii:\n\n"+
+        
+        "• Tipul activităţii desfăşurate;\n"+
+        "• Tipul vibraţiei transmise omului (mana-brat sau întregul corp);\n"+
+        "• Durata expunerii la vibraţii.        De exemplu: muncitor în construcţii, lucrează la picamer (vibraţie tip mana-brat) timp de 4 ore.",
     },
     {
         id: "e-learning",
@@ -312,7 +322,19 @@ export const servicesInfo = [
                 text: "Portal destinat angajaților, iar separat, unul pentru responsabilul SSM/ manager.",
             },
         ],
-        information: "",
+        information: '#style{"fontWeight":"bold","fontSize":22, "textDecoration":"underline"}APLICAȚIE PC/IOS/ANDROID SSM, SU ȘI RSVTI#/\n\n'+
+        "Pentru fiecare angajat, indiferent de statut, pot fi văzute toate acțiunile privind:\n\n"+
+        "- Vizualizarea și descărcarea documentelor;\n"+
+        "- Statusul privind parcurgerea instructajelor;\n"+
+        "- Ultimele versiuni de documente accesate;\n"+
+        "- Angajații care au susținut testele și punctajul obținut;\n"+
+        "- Angajații care nu au dat testele și data pentru care au fost reprogramați pentru susținerea testelor.",
+
+        information2: "Toate documentele pot fi vizualizate, descărcate și modificate în funcție de cerințele beneficiarului sau de obligațiile legale.\n"+
+        "Instructajele pot fi modificate și personalizate oricând, în funcție de tehnologii noi, riscuri noi, norme legislative noi etc."+
+        "Testele pot fi modificate în orice moment, în funcție de instructajele parcurse de angajați sau privind noi riscuri apărute, de exemplu riscul de infectare cu SARS-CoV-2. \n\n"+
+        "Mai mult, reprezentanții Work Management pot face verificări/checklist-uri privind respectarea normelor SSM, SU, PC, RSVTI și Mediu în sediile/punctele de lucru ale Beneficiarului.\n\n"+
+        '#style{"fontWeight":"bold", "color":"red", "display":"center"}TOATE ACESTE FUNCȚII ALE APLICAȚIEI NU SUNT LIMITATIVE, POT FI ADUSE ACTUALIZĂRI PENTRU A RĂSPUNDE TUTUROR NEVOILOR BENEFICIARULUI.#/',
     },
     {
         id: "audit-si-evaluarea-riscurilor",
@@ -334,7 +356,32 @@ export const servicesInfo = [
                 text: "Procedura de evaluare a riscurilor profesionale.",
             },
         ],
-        information: "",
+        information: '#style{"fontWeight":"bold", "fontSize":21, "marginLeft": "20px"}AUDIT#/\n\n'+
+        'Prin auditul SSM, experții #style{"fontWeight":"bold","textDecoration":"italic"}Work Management#/ vor evalua, modul în care compania dumneavoastră respectă reglementările legale în vigoare, în domeniul în care vă desfășurați activitatea.\n'+
+        "În urma auditului, veți primi un plan de măsuri tehnice și organizatorice, pentru ca dumneavoastră să vă desfașurati activitatea, în deplină siguranță.\n\n"+
+        "Vom verifica:\n"+
+ 
+        "- existența documentaţiei și conformarea cu activitățile prestate și locurile de muncă existente;\n"+
+        "- modul în care angajații dumneavoastră cunosc responsabilităţile în domeniul Securităţii şi Sănătăţii în Muncă;\n"+
+        "- respectarea cerinţelor minime referitoare la echipamentele de muncă și a echipamentelor individuale de protecţie;\n"+
+        "- respectarea cerinţelor minime privind mediul de muncă;\n"+
+        "- modul în care se efectuează manipularea maselor;\n"+
+        "- dacă este implementată semnalizarea minimă de securitate;\n"+
+        "- modul în care se desfășoară supravegherea sănătăţii lucrătorilor;\n"+
+        "- alte aspecte, în funcție de caz și de activitatea dumneavoastră (cerinţelor minime privind şantierele temporare şi mobile, cerinţelor minime privind protecţia maternităţii, etc)\n\n"+
+
+        'Suplimentar, vă putem oferi suport pentru implemenatarea certificării #style{"fontWeight":"bold"} ISO 9001:2015, ISO 14001:2015, ISO 45001:2018, SMSI ISO 27001.#/',
+        information2:'#style{"fontWeight":"bold","fontSize":21, "marginLeft": "20px"}EVALUAREA RISCURILOR#/\n\n'+
+         'Prin această lucrare, experții Work Management, #style{"fontWeight":"bold"}identifică#/ factorii de risc existenţi în cadrul companiei dumneavoastră şi #style{"fontWeight":"bold"}evaluează#/ dimensiunea acestora, raportandu-se la  importanța și frecvența consecinţelor, asupra securității și sănătății angajaţilor.\n\n'+
+        '#style{"fontWeight":"bold"}Evaluarea riscurilor de accidentare și îmbolnăvire profesională#/, are că scop identificarea și punerea în practică a masurilor pentru riscurile identificate în ordinea desccrescatoarea nivelului parțial de risc. Alegerea metodei de evaluare depinde de: tipul activității și al echipamentelor utilizate, numărul de lucrători implicați, condițiile de muncă, caracteristicile particulare ale locului de muncă și riscurile specifice.\n'+
+        'Ulterior, se intocmeste #style{"fontWeight":"bold"}Planul de Prevenire și Protecție#/, pe baza riscurilor identificate la locurile de muncă / posturile de lucru, specifice activităților desfășurate și a echipamentelor de muncă utilizate în cadrul societății.\n'+
+       ' #style{"fontWeight":"bold"} Procedura de evaluare a riscurilor profesionale#/ poate fi rezumată după cum urmează:\n\n'+
+        "- Identificarea pericolului\n"+
+        "- Eliminarea pericolului, dacă este rezonabil posibil\n"+
+        "- Analizarea pericolului, dacă acesta nu poate fi eliminat și evaluarea riscului\n"+
+        "- Luarea de măsuri pentru a limita consecințele\n"+
+        "- Supravegherea riscurilor\n"+
+        "- Metode și instrumente de evaluare a riscurilor.",
     },
     {
         id: "indicatoare",
