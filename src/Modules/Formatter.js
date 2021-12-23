a30e286e179aa751c6f8a573fe3267ee72fdbdf4
Bogdan <bogdanmarcu2003@gmail.com>
2021-12-23 18:29:59 +0200

    Fix

--- src/Modules/Formatter.js
+++ src/Modules/Formatter.js
@@ -25,34 +25,38 @@ import { Stack } from "../Modules/Stack";
 export function formatText(string) {
     let v = pf(string);
 
-    return (
-        <p>
-            <span>{string.substring(0, v[0].start - v[0].tagLength)}</span>
-            {v.map((data, index) => {
-                const renderPrefix = () => {
-                    if (index > 0) {
-                        return (
-                            <span key={index + ".first"}>
-                                {string.substring(
-                                    v[index - 1].end + 2,
-                                    v[index].start - v[index].tagLength
-                                )}
-                            </span>
-                        );
-                    }
-                };
-                return [
-                    renderPrefix(),
-                    <span key={index + ".second"} style={v[index].params}>
-                        {string.substring(v[index].start, v[index].end)}
-                    </span>,
-                ];
-            })}
-            <span>
-                {string.substring(v[v.length - 1].end + 2, string.length)}
-            </span>
-        </p>
-    );
+    if (v.length > 0) {
+        return (
+            <p>
+                <span>{string.substring(0, v[0].start - v[0].tagLength)}</span>
+                {v.map((data, index) => {
+                    const renderPrefix = () => {
+                        if (index > 0) {
+                            return (
+                                <span key={index + ".first"}>
+                                    {string.substring(
+                                        v[index - 1].end + 2,
+                                        v[index].start - v[index].tagLength
+                                    )}
+                                </span>
+                            );
+                        }
+                    };
+                    return [
+                        renderPrefix(),
+                        <span key={index + ".second"} style={v[index].params}>
+                            {string.substring(v[index].start, v[index].end)}
+                        </span>,
+                    ];
+                })}
+                <span>
+                    {string.substring(v[v.length - 1].end + 2, string.length)}
+                </span>
+            </p>
+        );
+    } else {
+        return <p>{string}</p>;
+    }
 }
 
 function pf(string) {
