4410bbfd1e00cb38b9065d56d65bb46615f022eb
Catalina <petruscatalina@yahoo.com>
2022-02-02 14:52:36 +0200

    cv

--- src/Screens/IndividualServicePage/Information/InfoService.js
+++ src/Screens/IndividualServicePage/Information/InfoService.js
@@ -132,8 +132,8 @@ export const servicesInfo = [
             },
         ],
         information:
-            '#style{"fontWeight":"bold", "fontSize": 22}SITUAȚII DE URGENȚĂ ( PSI și PC)#/\n\n' +
-            '#style{"fontWeight":"bold"}Work Management#/ oferă în domeniul prevenirea şi stingerea incendiilor (PSI) consultanţă de specialitate în conformitate cu normele metodologice de aplicare a prevederilor din Legea Nr. 307/2006.\n\n' +
+            '#style{"fontWeight":"bold", "fontSize": 22}SITUAȚII DE URGENȚĂ (Prevenirea și Stingerea incendiilor / Protecție civilă)#/\n\n' +
+            '#style{"fontWeight":"bold"}Work Management#/ oferă în domeniul Situațiilor de Urgență (PSI) consultanţă de specialitate în conformitate cu normele metodologice de aplicare a prevederilor din Legea Nr. 307/2006.\n\n' +
             '#style{"fontWeight":"bold"}a. Instructajul introductiv general:#/\n' +
             '#style{"marginLeft":"25px"}•sistemul de reglementări din domeniul situațiilor de urgență;#/\n' +
             '#style{"marginLeft":"25px"}•indicativ, mijloace tehnice în domeniul S.U., din dotarea societății;#/\n' +
@@ -280,9 +280,12 @@ export const servicesInfo = [
         information:
             '#style{"fontWeight":"bold", "fontSize": 21}Vă oferim:#/\n\n' +
             "• Servicii de Coordonare SSM prin serviciu extern de prevenire și protecție astfel încât să vă conformați în totalitate cu HG 300/2006 privind cerinţele minime de securitate şi sănătate pe şantierele temporare şi mobile care stipulează că obligativitate, desemnarea unui coordonator de securitate şi elaborarea planului de securitate și sănătate atât în faza de întocmire a proiectului cât și pe perioada execuției lucrărilor.\n" +
-            "• Întocmire Plan de Securitate și Sănătate în faza de proiectare\n" +
-            "• Întocmire Plan de Securitate și Sănătate în faza de execuție\n" +
-            "• Întocmire Plan Propriu de Securitate și Sănătate al Antreprenorului",
+            '• Întocmire #style{"fontWeight":"bold"}Plan de Securitate și Sănătate#/ în faza de proiectare;\n' +
+            "• Întocmire Plan de Securitate și Sănătate în faza de execuție;\n" +
+            "• Întocmire Plan Propriu de Securitate și Sănătate al Antreprenorului"+
+            "• Întocmirea declarației prealabile și comunicarea acesteia inspectoratului teritorial de muncă pe raza căruia se vor desfăşura lucrarile;\n "+
+            ' • Întocmire #style{"fontWeight":"bold"}Registru de coordonare;#/\n'+
+            ' • Întocmire #style{"fontWeight":"bold"}Dosar de intervenții ulterioare.#/',
         image: santiere,
         main_icon: <FaTools></FaTools>,
     },
@@ -307,9 +310,9 @@ export const servicesInfo = [
             },
         ],
         information:
-            "Obligativitatea analizei de risc, este prevazută, de Legea 333/2003 privind paza obiectivelor, bunurilor, valorilor și protecția persoanelor, pentru care s-au emis în anul 2012 normele de aplicare, prin hotărârea de guvern 301/2012.\n" +
+            '#style{"fontWeight":"bold", "color":"red"}Obligativitatea analizei de risc#/, este prevazută, de Legea 333/2003 privind paza obiectivelor, bunurilor, valorilor și protecția persoanelor, pentru care s-au emis în anul 2012 normele de aplicare, prin Hotărârea de Guvern 301/2012.\n' +
             "Evaluarea de risc, la securitatea fizică, este documentul, prin care, un specialist analizează și stabilește, de ce anume este nevoie, pentru ca paza obiectivelor dumneavoastră, să acopere toate vulnerabilitatile acestuia, în fața hoților.\n" +
-            "Toate societățile comerciale, sunt obligate să contracteze, un evaluator de risc, la securitat fizică, care să realizeze analiza de risc. Aceasta urmează să fie efectuată, pentru toate obiectele de activitate active și sediile dumneavoastră.\n\n" +
+            "Toate societățile comerciale, sunt obligate să contracteze, un evaluator de risc, la securitate fizică, care să realizeze analiza de risc. Aceasta urmează să fie efectuată, pentru toate obiectele de activitate active și sediile dumneavoastră.\n\n" +
             'Toate societățile comerciale, sunt obligate să contracteze, un evaluator de risc, la securitate fizică, care să realizeze analiză de risc. Aceasta urmează să fie efectuată, pentru toate obiectele de activitate active și sediile dumneavoastră. \n\n Societatea noastră, este abilitată, să întocmească astfel de evaluări, fiind înscrisă, în #style{"fontWeight":"bold"}Registrul Național al Evaluatorilor de Risc, la Securitate Fizică#/',
         image: risc,
         main_icon: <IoAnalyticsSharp></IoAnalyticsSharp>,
@@ -411,7 +414,7 @@ export const servicesInfo = [
             {
                 icon: <IoPeople size={40}></IoPeople>,
                 name: "O aplicație pentru întreaga companie",
-                text: "Portal destinat angajaților, iar separat, unul pentru responsabilul SSM/ manager.",
+                text: "Portal destinat testării angajaților tăi, iar separat, unul pentru responsabilul SSM/ manager.",
             },
         ],
         information:
@@ -426,7 +429,7 @@ export const servicesInfo = [
         information2:
             "Toate documentele pot fi vizualizate, descărcate și modificate în funcție de cerințele beneficiarului sau de obligațiile legale.\n" +
             "Instructajele pot fi modificate și personalizate oricând, în funcție de tehnologii noi, riscuri noi, norme legislative noi etc." +
-            "Testele pot fi modificate în orice moment, în funcție de instructajele parcurse de angajați sau privind noi riscuri apărute, de exemplu riscul de infectare cu SARS-CoV-2. \n\n" +
+            '\n\n#style{"fontWeight":"bold", "color":"red"}Testele pot fi modificate#/ în orice moment, în funcție de instructajele parcurse de angajați sau privind noi riscuri apărute#style{"fontWeight":"bold","color":"red"}!#/(de exemplu: riscul de infectare cu SARS-CoV-2.) \n\n' +
             "Mai mult, reprezentanții Work Management pot face verificări/checklist-uri privind respectarea normelor SSM, SU, PC, RSVTI și Mediu în sediile/punctele de lucru ale Beneficiarului.\n\n" +
             '#style{"fontWeight":"bold", "color":"red", "display":"center"}TOATE ACESTE FUNCȚII ALE APLICAȚIEI NU SUNT LIMITATIVE, POT FI ADUSE ACTUALIZĂRI PENTRU A RĂSPUNDE TUTUROR NEVOILOR BENEFICIARULUI.#/',
         image: e_learning,
@@ -439,7 +442,7 @@ export const servicesInfo = [
             {
                 icon: <FaHandsHelping size={40}></FaHandsHelping>,
                 name: "Suport",
-                text: "în implemenatarea certificărilor ISO ",
+                text: "Implemenatarea certificărilor ISO ",
             },
             {
                 icon: <RiOrganizationChart size={40}></RiOrganizationChart>,
@@ -457,7 +460,7 @@ export const servicesInfo = [
             },
         ],
         information:
-            '#style{"fontWeight":"bold", "fontSize":21, "marginLeft": "20px"}AUDIT#/\n\n' +
+            '#style{"fontWeight":"bold", "fontSize":21, "marginLeft": "25px", "color":"rgb(6, 6, 114)"}AUDIT#/\n\n' +
             'Prin auditul SSM, experții #style{"fontWeight":"bold","textDecoration":"italic"}Work Management#/ vor evalua, modul în care compania dumneavoastră respectă reglementările legale în vigoare, în domeniul în care vă desfășurați activitatea.\n' +
             "În urma auditului, veți primi un plan de măsuri tehnice și organizatorice, pentru ca dumneavoastră să vă desfașurati activitatea, în deplină siguranță.\n\n" +
             "Vom verifica:\n" +
@@ -471,10 +474,10 @@ export const servicesInfo = [
             "- alte aspecte, în funcție de caz și de activitatea dumneavoastră (cerinţelor minime privind şantierele temporare şi mobile, cerinţelor minime privind protecţia maternităţii, etc)\n\n" +
             'Suplimentar, vă putem oferi suport pentru implemenatarea certificării #style{"fontWeight":"bold"} ISO 9001:2015, ISO 14001:2015, ISO 45001:2018, SMSI ISO 27001.#/',
         information2:
-            '#style{"fontWeight":"bold","fontSize":21, "marginLeft": "20px"}EVALUAREA RISCURILOR#/\n\n' +
+            '#style{"fontWeight":"bold","fontSize":21, "marginLeft": "25px", "color":"rgb(6, 6, 114)"}EVALUAREA RISCURILOR#/\n\n' +
             'Prin această lucrare, experții Work Management, #style{"fontWeight":"bold"}identifică#/ factorii de risc existenţi în cadrul companiei dumneavoastră şi #style{"fontWeight":"bold"}evaluează#/ dimensiunea acestora, raportandu-se la  importanța și frecvența consecinţelor, asupra securității și sănătății angajaţilor.\n\n' +
-            '#style{"fontWeight":"bold"}Evaluarea riscurilor de accidentare și îmbolnăvire profesională#/, are că scop identificarea și punerea în practică a masurilor pentru riscurile identificate în ordinea desccrescatoarea nivelului parțial de risc. Alegerea metodei de evaluare depinde de: tipul activității și al echipamentelor utilizate, numărul de lucrători implicați, condițiile de muncă, caracteristicile particulare ale locului de muncă și riscurile specifice.\n' +
-            'Ulterior, se intocmeste #style{"fontWeight":"bold"}Planul de Prevenire și Protecție#/, pe baza riscurilor identificate la locurile de muncă / posturile de lucru, specifice activităților desfășurate și a echipamentelor de muncă utilizate în cadrul societății.\n' +
+            '#style{"fontWeight":"bold"}Evaluarea riscurilor de accidentare și îmbolnăvire profesională#/, are ca și scop identificarea și punerea în practică a măsurilor pentru riscurile identificate în ordine descrescătoare a nivelului parțial de risc. Alegerea metodei de evaluare depinde de: tipul activității și al echipamentelor utilizate, numărul de lucrători implicați, condițiile de muncă, caracteristicile particulare ale locului de muncă și riscurile specifice.\n' +
+            'Ulterior, se intocmeste #style{"fontWeight":"bold"}Planul de Prevenire și Protecție#/, pe baza riscurilor identificate la locurile de muncă / posturile de lucru, specifice activităților desfășurate și a echipamentelor de muncă utilizate în cadrul societății.\n\n' +
             ' #style{"fontWeight":"bold"} Procedura de evaluare a riscurilor profesionale#/ poate fi rezumată după cum urmează:\n\n' +
             "- Identificarea pericolului\n" +
             "- Eliminarea pericolului, dacă este rezonabil posibil\n" +
@@ -482,7 +485,7 @@ export const servicesInfo = [
             "- Luarea de măsuri pentru a limita consecințele\n" +
             "- Supravegherea riscurilor\n" +
             "- Metode și instrumente de evaluare a riscurilor.\n\n" +
-            "Evaluarea riscurilor de accidente și îmbolnăviri profesionale se face pentru fiecare loc de muncă (angajați care sunt expuși la același risc având activități similare sau apropiate).",
+            "Evaluarea riscurilor de accidente și îmbolnăviri profesionale se face pentru fiecare loc de muncă (angajații care sunt expuși la același risc, având activități similare sau apropiate).",
         image: evaluarea_riscurilor,
         main_icon: <FaUserInjured></FaUserInjured>,
     },
